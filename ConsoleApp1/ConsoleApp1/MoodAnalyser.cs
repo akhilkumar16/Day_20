2b5f493b724962ba212f051a76393fd9bed6a151
Your Name <akhilkumarchandragiri@gmail.com>
2022-01-02 20:35:25 +0530

    test

---
+++ ConsoleApp1/ConsoleApp1/MoodAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserproject
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood()
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
